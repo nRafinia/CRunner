c3518724dfce7019a0c34e74ec88a9c0d2e1f95a
Naser Rafinia <Naser.r@gmail.com>
2021-12-10 20:19:15 +0330

    add ioc

--- src/SSH/Program.cs
+++ src/SSH/Program.cs
@@ -1,94 +1,13 @@
-﻿using Renci.SshNet;
+﻿using Microsoft.Extensions.DependencyInjection;
+using SSH;
+using SSH.Providers.Ssh;
 
-Console.OutputEncoding = System.Text.Encoding.UTF8;
-Console.CursorVisible = false;
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<Startup>()
+    .AddSingleton<SshService>()
+    .BuildServiceProvider();
 
-Console.WriteLine("Starting...");
-Console.WriteLine("");
+var startup = serviceProvider.GetService<Startup>();
 
-var ips = File.ReadAllLines("ip.txt");
-var commands = File.ReadAllLines("command.txt");
-var sec = File.ReadAllLines("sec.txt");
+await startup.Start();
 
-foreach (var ip in ips)
-{
-    var connectionInfo = new ConnectionInfo(ip,
-                                            22,
-                                            sec[0],
-                                            new PasswordAuthenticationMethod(sec[0], sec[1])
-                                            /*new PrivateKeyAuthenticationMethod("rsa.key")*/);
-
-    try
-    {
-
-        using var client = new SshClient(connectionInfo);
-
-        client.Connect();
-
-        if (client.IsConnected)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"Connected to {ip}");
-            Console.WriteLine("");
-        }
-        else
-        {
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("Could not connect, disconnected.");
-            break;
-        }
-
-        Console.ForegroundColor = ConsoleColor.Gray;
-        var stream = client.CreateShellStream("Customcommand", 0, 0, 0, 0, 10);//1024);
-        //var reader = new StreamReader(stream);
-
-        stream.DataReceived += (sender, eventArgs) =>
-        {
-            var data = System.Text.Encoding.UTF8.GetString(eventArgs.Data);
-            Console.Write(data);
-        };
-
-        await Task.Delay(1000);
-
-        for (var i = 2; i < sec.Length; i++)
-        {
-            var cmd = sec[i];
-
-            stream.WriteLine(cmd);
-            await stream.FlushAsync();
-        }
-
-        foreach (var cmd in commands)
-        {
-            if (cmd.StartsWith("sleep"))
-            {
-                var sleep = cmd.Split(":");
-                await Task.Delay(int.Parse(sleep[1]));
-                continue;
-            }
-
-            stream.WriteLine(cmd);
-            await stream.FlushAsync();
-
-        }
-
-        client.Disconnect();
-
-        Console.ForegroundColor = ConsoleColor.Magenta;
-        Console.WriteLine("");
-        Console.WriteLine($"Disconnected from {ip}");
-
-        await Task.Delay(1000);
-    }
-    catch (Exception e)
-    {
-        Console.ForegroundColor = ConsoleColor.DarkRed;
-        Console.WriteLine("");
-        Console.WriteLine($"Error in connect to {ip}. Error={e.Message}");
-    }
-
-    Console.WriteLine("");
-}
-
-Console.ForegroundColor = ConsoleColor.Gray;
-Console.WriteLine("Finished.");
---
+++ src/SSH/Startup.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SSH.Providers.Ssh;
+
+namespace SSH;
+
+public class Startup
+{
+    private string[] ips;
+    private string[] commands;
+    private string[] sec;
+
+    private readonly IServiceProvider _provider;
+    public Startup(IServiceProvider provider)
+    {
+        _provider = provider;
+    }
+
+    public async Task Start()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.CursorVisible = false;
+
+        Console.WriteLine("Starting...");
+        Console.WriteLine("");
+
+        await LoadFiles();
+
+        foreach (var ip in ips)
+        {
+            try
+            {
+                var ssh = _provider.GetService<SshService>();
+                ssh.Connect(ip, sec[0], sec[1]);
+                await ssh.Run(sec, commands);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("");
+                Console.WriteLine($"Error in connect to {ip}. Error={e.Message}");
+            }
+
+            Console.WriteLine("");
+        }
+
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("Finished.");
+    }
+
+    private async Task LoadFiles()
+    {
+        ips = await File.ReadAllLinesAsync("ip.txt");
+        commands = await File.ReadAllLinesAsync("command.txt");
+        sec = await File.ReadAllLinesAsync("sec.txt");
+
+    }
+}
