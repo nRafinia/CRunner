814695c6c26aa52be8689d8990f77d18dc4e10e0
Naser Rafinia <Naser.r@gmail.com>
2021-12-13 15:34:42 +0330

    rename folder

---
+++ src/CRunner/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SSH;
+using SSH.Providers.Telnet;
+
+
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<Startup>()
+    //.AddSingleton<SshService>()
+    .AddSingleton<TelnetService>()
+    .BuildServiceProvider();
+
+var startup = serviceProvider.GetService<Startup>();
+
+await startup.Start();
+
